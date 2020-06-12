e24ce10f549ec6e4b1bc4ba67c1725e2d3bda413
Anita Gimenez Kesseru <anita@Anitas-MacBook-Pro.local>
2020-06-12 14:50:02 -0300

    buscador casi terminado en la mayoria de pags

---
+++ resultados.js
@@ -0,0 +1,45 @@
+window.addEventListener("load",function(){
+    let queryString = new URLSearchParams(location.search)
+
+    let loBuscado = queryString.get ("buscador");
+
+    fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/search?q=" + loBuscado + "")
+    .then(
+        function (respuesta){
+            return respuesta.json();
+
+        }
+    )
+
+    .then(
+        function (resultado){
+
+            resultado = resultado.data
+         console.log(resultado)
+
+         if (gifsPopulares.length == 0) {
+            alert("No hay resultados");
+        }
+
+        for (let index = 0; index < 1; index++) {
+            const cadaResultado = resultado[index];
+            
+            let nombreTrack = cadaResultado.title
+            let nombreArtista = cadaResultado.artist.name
+            let nombreAlbum = cadaresultado.album.title
+            let fotoTrack = cadaResultado.album.cover_medium
+            let duracion = cadaResultado.duration
+            let trackID = cadaResultado.index
+
+            let resultadoBusqueda = `
+            
+        `
+            
+            
+           
+        }
+
+       
+        }  
+    )
+})
