324d164615c49275a74f9b6a56fdb69c26914658
Luisa <luisa@MacBook-Pro-de-Luisa.local>
2020-06-12 14:48:38 -0300

    detalle cancion. falta css

--- detallecancion.js
+++ detallecancion.js
@@ -1,6 +1,7 @@
 window.addEventListener ("load", function(){
     let queryString = new URLSearchParams (location.search)
     let codigoCancion = queryString.get ("idDeTrack")
+//conecto con el URL de deezer
     fetch ("https://cors-anywhere.herokuapp.com/https://api.deezer.com/track/" + codigoCancion )
     .then(
         function (respuesta) {
@@ -14,22 +15,44 @@ window.addEventListener ("load", function(){
         let laCancion = information;
         console.log (information)
 
-        
+ // creo las variables para reemplazar en c/ cancion
        let nombreCancion = laCancion.title;
-       let artistaCancion = laCancion.artist;
-       let albumCancion = laCancion.album;
+       let artistaCancion = laCancion.artist.name;
+       let albumCancion = laCancion.album.title;
+       let imagenCancion = laCancion.album.cover_big
+       let añoCancion = laCancion.release_date
+       let tiempoCancion = laCancion.duration
    
-       
+// reemplazo variables en las etiquetas HTML
+     let nuevoTitulo = ` 
+     <h1> ` + nombreCancion + `  <br> ` + artistaCancion  + ` </h1> `
 
+// modifico HTML
+     document.querySelector (".titulocancion").innerHTML += nuevoTitulo
 
-   
-    document.querySelector (".unica").innerHTML += htmlNuevoCancion
-        
-        
-        
+     let nuevoAño = ` <h3> ` + añoCancion + ` </h3> `
 
-        
+     document.querySelector (".año").innerHTML += nuevoAño
 
+     let nuevoTiempo =` <h4> ` + tiempoCancion  + ` </h4> `
+    document.querySelector (".tiempo").innerHTML += nuevoTiempo
+
+    let nombre = `<p> ` + nombreCancion + ` </p>  `
+    document.querySelector (".song").innerHTML += nombre
+
+    let artista = ` <p> ` + artistaCancion + ` </p> `
+    document.querySelector (".artist").innerHTML += artista
+
+    let album = `<p> ` + albumCancion + ` <p> `
+    document.querySelector (".albums").innerHTML += album
+
+    let tiempo = ` <p> ` + tiempoCancion + ` </p> `
+
+    document.querySelector (".minutos").innerHTML += tiempo
+
+    let imagenDeLaCancion = ` <img class="img" src=" ` + imagenCancion + ` " alt=""> `
+
+    document.querySelector (".img").innerHTML += imagenDeLaCancion
 
            
         }
