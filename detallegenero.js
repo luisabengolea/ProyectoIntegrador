dd91dbe1ebcf587c698a2be9e7196c8a3f94ed42
Luisa <luisa@MacBook-Pro-de-Luisa.local>
2020-06-17 10:27:23 -0300

    genero

--- detallegenero.js
+++ detallegenero.js
@@ -19,12 +19,28 @@ window.addEventListener("load",function(){
 
          
 
-        
+         for (let i = 0; i < porGeneros.length; i ++){
+            const cadaGenero = porGeneros[i];
             
-         let genreImg = cadaGenero.picture_big;
-         let genreName = cadaGenero.name;
-         let genreId = cadaGenero.id;
-         
+            let genreImg = cadaGenero.picture_big;
+            let genreName = cadaGenero.name;
+            let genreId = cadaGenero.id;
+
+            let htmlNuevoGenero = `                     
+        <li>
+            <div class="slide">
+              <a href="detallegenero.html?iddeGeneros=` + genreId +`">
+              <img class="imgslide" src="`+ genreImg + `" alt="">
+              <div class="uk-position-center uk-panel "><h3 class="textoslide2">` + genreName + `</h3></div>
+              </a>
+            </div>
+        </li>
+            
+                `
+            
+            
+            document.querySelector(".listadoGeneros").innerHTML += htmlNuevoGenero
+        }
                 
             
             document.querySelector(".listadoGeneros").innerHTML += htmlNuevoGenero
