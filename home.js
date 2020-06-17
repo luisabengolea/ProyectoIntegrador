b2b94375bd9eb10a8e7d598a09918e7c89f36dd7
Anita Gimenez Kesseru <anita@Anitas-MacBook-Pro.local>
2020-06-17 14:19:45 -0300

    arreglos version mobile

--- home.js
+++ home.js
@@ -12,15 +12,22 @@ window.addEventListener("load",function() {
          
            
         let artistasPopulares = informacion.artists.data;
+            // console.log aca para ver que contiene la infromacion de data de charts 
 
-       // Trabaja el resultado  y armar vafiables con los reusltados para tenerlos almacenados 
+       // Trabaja el resultado  y armar variables con los reusltados para tenerlos almacenados 
 
             for (let i = 0; i < artistasPopulares.length; i ++){
                 const cadaArtista = artistasPopulares[i];
+
+                // este for recorre todos los artistas "tredning" que hay para despues pode imprimirlos depende de su id 
+                // muestra desde cuando tiene que empezar, el largo que seria la cantidad de artistas que hay y de cuanto en cuanto 
+                // const representa que despues el index se va a reemplazar por la info de los artistas 
                 
                 let artistImg = cadaArtista.picture_big;
                 let artistName = cadaArtista.name;
                 let artistId = cadaArtista.id;
+
+                // armo variables 
 
                 let htmlNuevoArtista = `
              <li>
@@ -32,15 +39,21 @@ window.addEventListener("load",function() {
                 </div>
             </li>
                     `
+
+                    // desde aca pongo la estuctura que seria de solo como se ve un artista, (es un li porque el carrousel es un ul con muchos li)
+                    // aca ya tengo armada esta estructur que se va a repetir siguiendo al cantidad de artistas que haya, 
                 
                 document.querySelector(".listadoArtistasPopulares").innerHTML += htmlNuevoArtista
             }
-            //elijo que parte del html quiero reemplazar 
+            //elijo que parte del html quiero reemplazar (EL UL)
+            //ACA BORRAMOS TODA LA ESTRUCTURA HECHA A MANO DEL HTML y reeptida Y DEJAMOS SOLO EL UL con clase que pusimos que se va a reemplazar automaticamente  
             
             
             
         }  
     )
+
+    //VOLVEMOS A HACER LO MISMO CON GENEROS Y TRACKS 
     fetch("https://cors-anywhere.herokuapp.com/https://api.deezer.com/chart")
     .then(
         function (respuesta){
